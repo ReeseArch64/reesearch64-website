686a909506ba7761775d0f8f1aa66a75a240e3bc
Alan Guerra <195439767+ReeseArch64@users.noreply.github.com>
2025-05-24 13:51:43 -0300

    alterando url

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -2,7 +2,7 @@ import type { ApiDataInterface } from "@/interfaces/ApiDataInterface";
 
 export async function getData(): Promise<ApiDataInterface> {
   const res = await fetch(
-    "https://raw.githubusercontent.com/ReeseArch64/reesearch64-api/refs/heads/main/api/PT/br/data.json",
+    "https://raw.githubusercontent.com/ReeseArch64/reesearch64-api/refs/heads/main/api/pt/br/data.json",
     // { next: { revalidate: 3600 } }
   );
 
